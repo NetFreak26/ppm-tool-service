0922612b266f5f5a559ee701565d23b4210fbb09
NetFreak26 <chirayugarg001999@gmail.com>
2022-09-19 10:37:04 +0530

    Setting up the project

---
+++ src/main/java/com/src/projectmanagementtoolservice/ProjectManagementToolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.src.projectmanagementtoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementToolServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/src/projectmanagementtoolservice/ProjectManagementToolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.projectmanagementtoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
